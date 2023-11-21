2640d5dcc6f92c2c294e3ffb890872d6265604fe
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-11-22 00:06:23 +0100

    🛠 Added 404 page

--- src/UI/src/app/app-routing.module.ts
+++ src/UI/src/app/app-routing.module.ts
@@ -1,27 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/service/auth-guard';
+import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 // Main routes
-
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+    loadChildren: () =>
+      import('./modules/layout/layout.module').then((m) => m.LayoutModule),
     canActivate: [AuthGuard],
   },
   {
     path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'courses',
-    loadChildren: () => import('./modules/management/management.module').then((m) => m.ManagementModule),
+    loadChildren: () =>
+      import('./modules/management/management.module').then(
+        (m) => m.ManagementModule
+      ),
     canActivate: [AuthGuard],
-
-  }, 
-  { path: '**', redirectTo: 'error/404' },
-
+  },
+  { path: '**', component: NotFoundComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
